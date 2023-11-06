eb93ae2eb089819a368c94564c667aa9ddaf82dc
nb.hmida <nb.hmida@instadeep.com>
2023-11-06 15:38:38 +0100

    Fix: Fixing dropping outliers implementation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,10 +27,14 @@ def go(args):
 
     # Dropping outliers based on the min and max values provided
     logger.info("Dropping outliers: keeping prices between %s and %s", args.min_price, args.max_price)
-    df = df[~df['price'].between(args.min_price, args.max_price)]
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
 
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
+
+    # Converting the last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
 
     # Saving the processed dataframe locally
     logger.info("Saving the processed dataframe locally")
